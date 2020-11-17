9ff65bcd53e13eb479d987ce288ffa10aa48bf35
Augen-Auf <an_lesiv@mail.ru>
2020-11-18 00:04:16 +0500

    added BIPIT_CW_8

---
+++ ConsoleHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(БИПиТ_КР_8.Service)))
+            {
+                Console.Title = "Server";
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
